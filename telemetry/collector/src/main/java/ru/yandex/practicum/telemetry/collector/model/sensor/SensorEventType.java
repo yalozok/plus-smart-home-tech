a63db1c6b8e6d1317407399516393cb38ff43e01
yalozok <olga.kozmovskaia@yandex.ru>
2025-08-23 21:38:01 +0200

    Add model SensorEvent

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/sensor/SensorEventType.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.telemetry.collector.model.sensor;
+
+public enum SensorEventType {
+    CLIMATE_SENSOR_EVENT, LIGHT_SENSOR_EVENT, MOTION_SENSOR_EVENT,
+    SWITCH_SENSOR_EVENT, TEMPERATURE_SENSOR_EVENT
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/sensor/TemperatureSensorEvent.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.telemetry.collector.model.sensor;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class TemperatureSensorEvent extends SensorEvent {
+    private int temperatureC;
+    private int temperatureF;
+
+    public SensorEventType getType() {
+        return SensorEventType.TEMPERATURE_SENSOR_EVENT;
+    }
+}
