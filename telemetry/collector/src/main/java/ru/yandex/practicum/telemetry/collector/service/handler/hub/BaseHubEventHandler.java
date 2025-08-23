bc20d80ee6645c7502a3d05a05c06159b107d4c8
yalozok <olga.kozmovskaia@yandex.ru>
2025-08-23 21:38:53 +0200

    Add Hub handlers

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/hub/BaseHubEventHandler.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.telemetry.collector.service.handler.hub;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.specific.SpecificRecordBase;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.telemetry.collector.configuration.KafkaTopic;
+import ru.yandex.practicum.telemetry.collector.model.hub.HubEvent;
+import ru.yandex.practicum.telemetry.collector.service.KafkaEventProducer;
+import ru.yandex.practicum.telemetry.collector.service.handler.HubEventHandler;
+
+@RequiredArgsConstructor
+public abstract class BaseHubEventHandler<T extends SpecificRecordBase> implements HubEventHandler {
+    protected final KafkaEventProducer producer;
+
+    protected abstract T mapToAvro(HubEvent event);
+
+    @Override
+    public void handle(HubEvent event) {
+        if (!event.getType().equals(getType())) {
+            throw new IllegalArgumentException("Event type mismatch");
+        }
+
+        T payload = mapToAvro(event);
+
+        HubEventAvro eventAvro = HubEventAvro.newBuilder()
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setPayload(payload)
+                .build();
+
+        producer.send(eventAvro, event.getHubId(), event.getTimestamp(), KafkaTopic.HUBS);
+    }
+}
