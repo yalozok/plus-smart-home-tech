e33d469eb16e6cb1233525071273490fc4d6dff7
yalozok <olga.kozmovskaia@yandex.ru>
2025-08-23 21:35:46 +0200

    Add model HubEvent

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/hub/ConditionOperation.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.telemetry.collector.model.hub;
+
+public enum ConditionOperation {
+    EQUALS,
+    GREATER_THAN,
+    LOWER_THAN
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/hub/HubEvent.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.telemetry.collector.model.hub;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.Instant;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        defaultImpl = HubEventType.class
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = DeviceAddedEvent.class, name = "DEVICE_ADDED"),
+        @JsonSubTypes.Type(value = DeviceRemovedEvent.class, name = "DEVICE_REMOVED"),
+        @JsonSubTypes.Type(value = ScenarioAddedEvent.class, name = "SCENARIO_ADDED"),
+        @JsonSubTypes.Type(value = ScenarioRemovedEvent.class, name = "SCENARIO_REMOVED")
+})
+@Getter
+@Setter
+@ToString
+public abstract class HubEvent {
+    private String hubId;
+    private Instant timestamp = Instant.now();
+
+    public abstract HubEventType getType();
+}
