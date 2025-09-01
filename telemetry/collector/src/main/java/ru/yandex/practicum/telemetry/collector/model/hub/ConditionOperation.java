52d2fb64a27a429a6122e29438f77843df9d017c
yalozok <olga.kozmovskaia@yandex.ru>
2025-09-01 13:13:17 +0200

    Remove JSON model

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/hub/ConditionOperation.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.telemetry.collector.model.hub;
-
-public enum ConditionOperation {
-    EQUALS,
-    GREATER_THAN,
-    LOWER_THAN
-}
--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/sensor/SensorEventType.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.practicum.telemetry.collector.model.sensor;
-
-public enum SensorEventType {
-    CLIMATE_SENSOR_EVENT, LIGHT_SENSOR_EVENT, MOTION_SENSOR_EVENT,
-    SWITCH_SENSOR_EVENT, TEMPERATURE_SENSOR_EVENT
-}
