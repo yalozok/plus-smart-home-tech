fc08f9e1a2c2e344aafe10e615227a6596757c7d
yalozok <olga.kozmovskaia@yandex.ru>
2025-09-07 12:18:11 +0200

    Remove redundant code

--- telemetry/analyzer/src/main/java/ru/yandex/grpc/analyzer/dal/repository/SensorRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/grpc/analyzer/dal/repository/SensorRepository.java
@@ -8,6 +8,4 @@ import java.util.Optional;
 
 public interface SensorRepository extends JpaRepository<Sensor, String> {
     boolean existsByIdInAndHubId(Collection<String> ids, String hubId);
-
-    Optional<Sensor> findByIdAndHubId(String id, String hubId);
 }
