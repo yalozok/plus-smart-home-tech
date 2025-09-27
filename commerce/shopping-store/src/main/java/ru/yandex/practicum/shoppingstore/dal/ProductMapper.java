27ccd725f6c3f4e28154f4c8d22e84d3434cbe58
yalozok <olga.kozmovskaia@yandex.ru>
2025-09-27 11:06:45 +0200

    Remove unused `toDtoList` method from `ProductMapper`

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/dal/ProductMapper.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/dal/ProductMapper.java
@@ -4,14 +4,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import ru.yandex.practicum.commerce.dto.shopping.store.ProductDto;
 
-import java.util.List;
-
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface ProductMapper {
     ProductDto toDto(Product product);
+
     Product toEntity(ProductDto productDto);
-    List<ProductDto> toDtoList(List<Product> products);
 }
