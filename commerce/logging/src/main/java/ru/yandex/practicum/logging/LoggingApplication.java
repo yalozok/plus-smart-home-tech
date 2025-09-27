a7499e49abb371fb7dd6160f3c575f024b4a1cd8
yalozok <olga.kozmovskaia@yandex.ru>
2025-09-27 11:44:43 +0200

    Remove unused import

--- commerce/logging/src/main/java/ru/yandex/practicum/logging/LoggingApplication.java
+++ commerce/logging/src/main/java/ru/yandex/practicum/logging/LoggingApplication.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class LoggingApplication {
