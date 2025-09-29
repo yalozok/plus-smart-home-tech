de02b368e43fb174f79274ce7d18210619a7b4c5
yalozok <olga.kozmovskaia@yandex.ru>
2025-09-29 10:54:26 +0200

    Remove unused import

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/contract/warehouse/exception/NoSpecifiedProductInWarehouseException.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/contract/warehouse/exception/NoSpecifiedProductInWarehouseException.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.commerce.contract.warehouse.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.UUID;
-
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NoSpecifiedProductInWarehouseException extends RuntimeException {
     public NoSpecifiedProductInWarehouseException(String message) {
