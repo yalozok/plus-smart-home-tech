d436deff55085ea3f35af4ca381d6349c2f41366
yalozok <olga.kozmovskaia@yandex.ru>
2025-10-18 16:09:34 +0200

    Update exceptions

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/contract/payment/exception/NoPaymentFoundException.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/contract/payment/exception/NoPaymentFoundException.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.commerce.contract.payment.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoPaymentFoundException extends RuntimeException {
     public NoPaymentFoundException(String message) {
         super(message);
